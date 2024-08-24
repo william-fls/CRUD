12e0a19033c0c29ebc8774792087d05b8257f413
Joédio Borges Jr <joedio.borges@ifc.edu.br>
2024-08-24 10:11:56 -0300

    Versão produtos e index

---
+++ controllers/categoriaController.js
@@ -0,0 +1,94 @@
+//crie o conteudo para este arquivo categoriaController.js com o seguinte conteudo: tabela de categorias com os campos: id, nome, preco, descricao, quantidade, categoria_id
+
+const Categoria = require('../models/categoriaModel');
+
+const categoriaController = {
+    createCategoria: (req, res) => {
+        const newCategoria = {
+            nome: req.body.nome,
+            descricao: req.body.descricao,
+            preco: req.body.preco,
+            quantidade: req.body.quantidade,
+            categoria: req.body.categoria,
+        };
+
+        Categoria.create(newCategoria, (err, categoriaId) => {
+            if (err) {
+                return res.status(500).json({ error: err });
+            }
+            res.redirect('/categorias');
+        });
+    },
+
+    getCategoriaById: (req, res) => {
+        const categoriaId = req.params.id;
+
+        Categoria.findById(categoriaId, (err, categoria) => {
+            if (err) {
+                return res.status(500).json({ error: err });
+            }
+            if (!categoria) {
+                return res.status(404).json({ message: 'Categoria not found' });
+            }
+            res.render('categorias/show', { categoria });
+        });
+    },
+
+    getAllCategorias: (req, res) => {
+        Categoria.getAll((err, categorias) => {
+            if (err) {
+                return res.status(500).json({ error: err });
+            }
+            res.render('categorias/index', { categorias });
+        });
+    },
+
+    renderCreateForm: (req, res) => {
+        res.render('categorias/create');
+    },
+
+    renderEditForm: (req, res) => {
+        const categoriaId = req.params.id;
+
+        Categoria.findById(categoriaId, (err, categoria) => {
+            if (err) {
+                return res.status(500).json({ error: err });
+            }
+            if (!categoria) {
+                return res.status(404).json({ message: 'Categoria not found' });
+            }
+            res.render('categorias/edit', { categoria });
+        });
+    },
+
+    updateCategoria: (req, res) => {
+        const categoriaId = req.params.id;
+        const updatedCategoria = {
+            nome: req.body.nome,
+            preco: req.body.preco,
+            descricao: req.body.descricao,
+            quantidade: req.body.quantidade,
+            categoria: req.body.categoria,
+        };
+
+        Categoria.update(categoriaId, updatedCategoria, (err) => {
+            if (err) {
+                return res.status(500).json({ error: err });
+            }
+            res.redirect('/categorias');
+        });
+    },
+
+    deleteCategoria: (req, res) => {
+        const categoriaId = req.params.id;
+
+        Categoria.delete(categoriaId, (err) => {
+            if (err) {
+                return res.status(500).json({ error: err });
+            }
+            res.redirect('/categorias');
+        });
+    }
+};
+
+module.exports = categoriaController;
